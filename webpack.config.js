db8b77c93ed0dc382d877c86c60e19fcf0bfbf5a
leecho <leecho87@naver.com>
2019-03-18 14:25:30 +0900

    *.js는 왜 안될까

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     entry : {
         app : ['./src/Model.js','./src/View.js','./src/Controller.js','./src/App.js']
+        // app : ['./src/*.js']
     },
     output : {
         filename : 'bundle.js',
